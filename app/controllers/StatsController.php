c11a6d8d39dc715f220216a6738ea73cbaf07919
Andy Awidarto <andy.awidarto@gmail.com>
2015-03-24 21:42:21 +0700

    hotspot

--- app/controllers/StatsController.php
+++ app/controllers/StatsController.php
@@ -106,22 +106,102 @@ class StatsController extends BaseReportController {
 
     }
 
-    public function postIndex()
+
+    public function getMerchant($mid = null)
     {
 
-        $this->fields = array(
-            array('title',array('kind'=>'text','query'=>'like','pos'=>'both','show'=>true)),
-            array('access',array('kind'=>'text','query'=>'like','pos'=>'both','show'=>true)),
-            array('docShare',array('kind'=>'text','query'=>'like','pos'=>'both','show'=>true,'callback'=>'splitShare')),
-            array('creatorName',array('kind'=>'text','query'=>'like','pos'=>'both','show'=>true,'attr'=>array('class'=>'expander'))),
-            array('docCategoryLabel',array('kind'=>'text','query'=>'like','pos'=>'both','show'=>true)),
-            array('docFilename',array('kind'=>'text','query'=>'like','pos'=>'both','show'=>true)),
-            array('docTag',array('kind'=>'text','query'=>'like','pos'=>'both','show'=>true,'callback'=>'splitTag')),
-            array('createdDate',array('kind'=>'datetime','query'=>'like','pos'=>'both','show'=>true)),
-            array('lastUpdate',array('kind'=>'datetime','query'=>'like','pos'=>'both','show'=>true)),
-        );
+        Breadcrumbs::addCrumb('Statistics',URL::to($this->controller_name));
 
-        return parent::postIndex();
+        $this->report_action = $this->controller_name;
+
+        $this->additional_filter = View::make('stats.addfilter')
+            ->with('report_action', $this->report_action)
+            ->render();
+
+            $daterange = Input::get('date_filter');
+
+            if($daterange == '' || is_null($daterange)){
+                $daterange = date('01-m-Y',time()).' - '.date('t-m-Y',time());
+            }
+
+            $daterange = explode(' - ', $daterange);
+            $start = Carbon::parse($daterange[0]);
+            $end = Carbon::parse($daterange[1]);
+            $end->addDay();
+
+            $timerange = array();
+
+            do{
+
+                $endday = clone($start);
+                $endday->addHours(23)->addMinutes(59)->addSeconds(59);
+                //print $start->toDateTimeString().' - '.$endday->toDateTimeString()."\r\n";
+
+                $timerange[] = array('start'=>$start->toDateTimeString(), 'end'=>$endday->toDateTimeString());
+
+                $start->addDay();
+            }while($start != $end);
+
+            $views = array();
+            $clicks = array();
+            $labels = array();
+            foreach ($timerange as $t) {
+                $from = new MongoDate( strtotime( $t['start'] ) );
+                $to = new MongoDate( strtotime( $t['end'] ) );
+
+                if(is_null($mid)){
+                    $clicks[] = Clicklog::whereBetween('clickedAt', array($from, $to) )->count();
+                    $views[] = Viewlog::whereBetween('viewedAt', array($from, $to) )->count();
+                }else{
+                    $clicks[] = Clicklog::whereBetween('clickedAt', array($from, $to) )
+                                    ->where('merchantId',$mid)
+                                    ->count();
+                    $views[] = Viewlog::whereBetween('viewedAt', array($from, $to) )
+                                    ->where('merchantId',$mid)
+                                    ->count();
+                }
+
+                $labels[] = date('d-m-Y', strtotime( $t['start'] ) );
+            }
+
+
+
+
+            //print_r($clicks);
+
+                $clickData = array(
+                    'label'=>'Clicks',
+                    'fillColor'=>'rgba(123,109,112,0.5)',
+                    'strokeColor'=>'rgba(123,109,112,1)',
+                    'pointColor'=>'rgba(123,109,112,1)',
+                    'pointStrokeColor'=>'#fff',
+                    'pointHighlightFill'=>'#fff',
+                    'pointHighlightStroke'=>'rgba(220,220,220,1)',
+                    'data'=>$clicks
+                );
+
+                $viewData = array(
+                    'label'=>'Views',
+                    'fillColor'=>'rgba(234,219,196,0.5)',
+                    'strokeColor'=>'rgba(234,219,196,1)',
+                    'pointColor'=>'rgba(234,219,196,1)',
+                    'pointStrokeColor'=>'#fff',
+                    'pointHighlightFill'=>'#fff',
+                    'pointHighlightStroke'=>'rgba(220,220,220,1)',
+                    'data'=>$views
+                );
+
+
+                $this->data = array(
+                    'series01'=>$viewData,
+                    'series02'=>$clickData,
+                    'labels'=>$labels
+                    );
+
+        $this->report_view = 'stats.report';
+        $this->title = 'Global Statistics';
+        return parent::getIndex();
+
     }
 
     public function makeActions($data)
